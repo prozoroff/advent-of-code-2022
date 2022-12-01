265cb1b0abd3485dedf2d1ce8356d9d4799f6594
Alex Prozorov <alexprozoroff@gmail.com>
2022-12-01 07:12:33 +0000

    Update day1.js

--- day1.js
+++ day1.js
@@ -16,15 +16,15 @@ const data =`
 `;
 
 const maxSnacks = count => data
-		.split('\n\n')
-		.map(nums => nums
-			.split('\n')
-			.filter(x => x)
-			.map(num => parseInt(num))
-			.reduce((acc,n)=>acc+n,0))
-		.sort((a,b) => a > b ? 1 : -1)
-		.slice(-count)
-		.reduce((acc,n)=>acc+n,0)
+	.split('\n\n')
+	.map(nums => nums
+		.split('\n')
+		.filter(x => x)
+		.map(num => parseInt(num))
+		.reduce((acc,n)=>acc+n,0))
+	.sort((a,b) => a > b ? 1 : -1)
+	.slice(-count)
+	.reduce((acc,n)=>acc+n,0)
 
 console.log('First puzzle answer:', maxSnacks(1));
 console.log('Second puzzle answer:', maxSnacks(3));
