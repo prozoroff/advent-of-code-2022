eda159aae585e5ea94613a3088a84a359cba1660
Alex Prozorov <alexprozoroff@gmail.com>
2022-12-05 07:18:55 +0000

    Create day5.js

---
+++ day5.js
@@ -0,0 +1,49 @@
+const data =`
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+`
+.split('\n\n')
+.map(p => p.split('\n')
+.filter(p => p));
+
+const parseData = data => data
+	.map((part, i) => i
+		? part
+			.map(line => line
+				.split(' ')
+				.filter((_, i) => i % 2)
+				.map(str => parseInt(str)))
+		: part
+			.filter((_, i) => i < part.length - 1)
+			.reduce((acc, line, i) => {
+				line
+					.replace(/    /g, '*')
+					.replace(/[\s\[\]]/g, '')
+					.split('')
+					.forEach((crate, k) => 
+						crate !== '*' && (acc[k] = acc[k] || []).push(crate))
+				return acc;
+			}, []));
+
+const moveCrates = sameOrder => {
+	const [columns, moves] = parseData(data);
+	moves.forEach(([count, from, to]) => {
+		const items = columns[from - 1].slice(0, count);
+		columns[from - 1] = columns[from - 1].slice(count);
+		columns[to - 1] = [
+			...(sameOrder ? items.reverse() : items),
+			...columns[to - 1]
+		];
+	});
+	return columns.map(c => c[0]).join('');
+} 
+
+console.log('First puzzle answer:', moveCrates(true));
+console.log('Second puzzle answer:', moveCrates(false));
