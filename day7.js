6dcfdc99088f14ca15dae8c0fb29fd000fc6d387
Alex Prozorov <alexprozoroff@gmail.com>
2022-12-07 07:40:29 +0000

    Update day7.js

--- day7.js
+++ day7.js
@@ -27,103 +27,103 @@ $ ls
 .filter(x => x);
 
 class Node {
-	constructor (name, parent, size) {
-		this.name = name;
-		this.parentNode = parent;
-		this.sizeValue = size;
+    constructor (name, parent, size) {
+        this.name = name;
+        this.parentNode = parent;
+        this.sizeValue = size;
 
-		if (parent) {
-			this.rootNode = parent.root()
-		}
+        if (parent) {
+            this.rootNode = parent.root()
+        }
 
-		if (!size) {
-			this.children = []
-		}
-	}
+        if (!size) {
+            this.children = []
+        }
+    }
 
-	root() {
-		return this.rootNode || this;
-	}
+    root() {
+        return this.rootNode || this;
+    }
 
-	parent() {
-		return this.parentNode || this;
-	}
+    parent() {
+        return this.parentNode || this;
+    }
 
-	child(name) {
-		return this.children
-			.find(c => c.name === name);
-	}
+    child(name) {
+        return this.children
+            .find(c => c.name === name);
+    }
 
-	addChild(child) {
-		this.children.push(child);
-	}
+    addChild(child) {
+        this.children.push(child);
+    }
 
-	size() {
-		return this.sizeValue 
-			|| this.children
-				.map(c => c.size())
-				.reduce((sum, size) => sum + size, 0);
-	}
+    size() {
+        return this.sizeValue 
+            || this.children
+                .map(c => c.size())
+                .reduce((sum, size) => sum + size, 0);
+    }
 
-	getDirSizes(acc = []) {
-		return this.children
-		?	[
-				...(this.children
-						.map(c => c.getDirSizes(acc))
-						.flat()),
-				this.size()
-			]
-		: acc;
-	}
+    getDirSizes(acc = []) {
+        return this.children
+        ?    [
+                ...(this.children
+                        .map(c => c.getDirSizes(acc))
+                        .flat()),
+                this.size()
+            ]
+        : acc;
+    }
 }
 
 const tree = data.reduce((curDir, line) => {
-	if(line[0] === '$') {
-		const [_, command, path] = line.split(' ');
-		if(command === 'cd') {
-			if (path === '/') {
-				return curDir.root() || curDir;
-			} else if (path === '..') {
-				return curDir.parent() || curDir;
-			} else {
-				let targetDir = curDir.child(path)
-				if (!targetDir) {
-					targetDir = new Node(path, curDir)
-					curDir.addChild(targetDir);
-				}
-				return targetDir;
-			}
-		}
-	} else {
-		const [sizeOrDir, name] = line.split(' ');
-		if (sizeOrDir === 'dir') {
-			let targetDir = curDir.child(name)
-			if (!targetDir) {
-				targetDir = new Node(name, curDir)
-				curDir.addChild(targetDir);
-			}
-		} else {
-			let file = curDir.child(name);
-			if (!file) {
-				file = new Node(name, curDir, parseInt(sizeOrDir));
-				curDir.addChild(file);
-			}
-		}
-	}
-	return curDir;
+    if(line[0] === '$') {
+        const [_, command, path] = line.split(' ');
+        if(command === 'cd') {
+            if (path === '/') {
+                return curDir.root() || curDir;
+            } else if (path === '..') {
+                return curDir.parent() || curDir;
+            } else {
+                let targetDir = curDir.child(path)
+                if (!targetDir) {
+                    targetDir = new Node(path, curDir)
+                    curDir.addChild(targetDir);
+                }
+                return targetDir;
+            }
+        }
+    } else {
+        const [sizeOrDir, name] = line.split(' ');
+        if (sizeOrDir === 'dir') {
+            let targetDir = curDir.child(name)
+            if (!targetDir) {
+                targetDir = new Node(name, curDir)
+                curDir.addChild(targetDir);
+            }
+        } else {
+            let file = curDir.child(name);
+            if (!file) {
+                file = new Node(name, curDir, parseInt(sizeOrDir));
+                curDir.addChild(file);
+            }
+        }
+    }
+    return curDir;
 }, new Node('/'))
 
 const result1 = tree
-	.root()
-	.getDirSizes()
-	.filter(v => v < 100000)
-	.reduce((acc, item) => acc + item, 0);
+    .root()
+    .getDirSizes()
+    .filter(v => v < 100000)
+    .reduce((acc, item) => acc + item, 0);
 
 const result2 = tree
-	.root()
-	.getDirSizes()
-	.filter(v => v > tree.root().size() - 40000000)
-	.sort((a,b) => a > b ? 1 : -1)[0];
+    .root()
+    .getDirSizes()
+    .filter(v => v > tree.root().size() - 40000000)
+    .sort((a,b) => a > b ? 1 : -1)[0];
 
 console.log('First puzzle answer:', result1);
 console.log('Second puzzle answer:', result2);
