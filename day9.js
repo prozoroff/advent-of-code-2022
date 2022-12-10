6e6f62959ea35f22c04d7ee99e89c7066e537461
Alex Prozorov <alexprozoroff@gmail.com>
2022-12-10 07:45:54 +0000

    Create day9.js

---
+++ day9.js
@@ -0,0 +1,43 @@
+const steps = `
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2
+`
+.split('\n')
+.filter(Boolean)
+.map(line => line.split(' '))
+.map(([direction, count]) => new Array(parseInt(count)).fill(direction)).flat()
+
+const moveHead = (rope, direction) => ({
+    R: coord => coord[0] += 1,
+    L: coord => coord[0] -= 1,
+    U: coord => coord[1] += 1,
+    D: coord => coord[1] -= 1
+})[direction](rope[0]);
+
+const moveChain = (rope, i) => {
+    const change = [rope[i - 1][0] - rope[i][0], rope[i - 1][1] - rope[i][1]];
+    if ((Math.abs(change[0]) === 2 || Math.abs(change[1]) === 2)) {
+        change.map((_, j) => rope[i][j] += Math.sign(change[j]));
+    }
+}
+
+const countVisited = ropeLength => {
+    const visited = {};
+    const rope = new Array(ropeLength).fill(0).map(() => [0, 0]);
+    steps.forEach(direction => {
+            moveHead(rope, direction);
+            rope.slice(1).forEach((_, i) => moveChain(rope, i + 1))
+            visited[rope[rope.length - 1]] = true;
+        });
+    return Object.keys(visited).length;
+}
+
+console.log('First puzzle answer:', countVisited(2));
+console.log('Second puzzle answer:', countVisited(10));
+
