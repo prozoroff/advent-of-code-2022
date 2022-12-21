2083b5684fb5539b4f084b80b6b336f48fed3b78
Alex Prozorov <alexprozoroff@gmail.com>
2022-12-21 21:13:03 +0000

    Update day20.js

--- day20.js
+++ day20.js
@@ -14,13 +14,13 @@ const numbers = `
 const move = (data, item) => {
     let from = data.indexOf(item);
     let shift = parseInt(item.split(',')[0]);
-    const size = data.length;
-    const to = ((from + shift) % (size-1) + (size-1)) % (size-1) || size-1;
+    const size = data.length - 1;
+    const to = ((from + shift) % size + size) % size || size;
     data.splice(from, 1);
     data.splice(to, 0, item);
 };
 
-const encrypt = (times, multiplier) => {
+const decrypt = (times, multiplier) => {
     const data = numbers.map((n, i) => `${n * multiplier},${i}`);
     const dataCopy = [...data];
     [...Array(times)].forEach(() => dataCopy.forEach(item => move(data, item)));
@@ -31,5 +31,5 @@ const encrypt = (times, multiplier) => {
             acc + parseInt(data[(zeroIndex + shift) % data.length].split(',')[0]), 0);
 }
 
-console.log('First puzzle answer:', encrypt(1, 1));
-console.log('Second puzzle answer:', encrypt(10, 811589153));
+console.log('First puzzle answer:', decrypt(1, 1));
+console.log('Second puzzle answer:', decrypt(10, 811589153));
